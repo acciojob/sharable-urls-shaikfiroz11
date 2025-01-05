c5de39133a215f8b18fd7dd76cbe7ba5436ec2f0
shaikfiroz11 <102669222+shaikfiroz11@users.noreply.github.com>
2025-01-05 22:48:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+const heading=document.getElementById("id");
+const name=document.getElementById("name");
+const year=document.getElementById("year");
+const submit=document.getElementById("button");
+
+submit.addEventListenner('click',()=>{
+	const Name=document.getElementById("name").value;
+	const Year=document.getElementById("year").value;
+	  if(Name!=="" &Year!==""){
+		  heading.innerHTML=`https://localhost:8080/?name=${Name}&year=${Year}`;
+	  }elseif(name===""){
+		   heading.innerHTML=`https://localhost:8080/?year=${Year}`
+	  }else{
+		   heading.innerHTML=`https://localhost:8080/?name=${Name}`
+	  }
+})
